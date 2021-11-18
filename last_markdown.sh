44a7d9ebedba471c340abfff885b8ca542ca895e
nueffel <pingu@benjamin-herb.de>
2021-11-18 11:06:18 +0100

    Added Filename updateds based on heading when working with the last opend md file

--- last_markdown.sh
+++ last_markdown.sh
@@ -1,11 +1,17 @@
-#!/bin/bash
+#!/bin/zsh
 
 dir=${HOME}/Notes
 
-unset -v latest
-for file in "$dir"/*; do
-  [[ $file -nt $latest ]] && latest=$file
-done
+filename=$(ls -tR $dir | grep .md | head -n 1)
+echo "Latest File: $filename"
+ 
+apostrophe "$dir/$filename"
 
-apostrophe $latest
+
+firstline=$(head -n 1 "$dir/$filename")
+if [[ "$firstline" =~ "# " ]]; then
+  fileending="${${firstline// /_}//\#/}"
+  mv "$dir/$filename" "$dir/${filename:0:13}${fileending}.md"
+fi
+
 
