32ab2935e81cb5024341610a4353adff8de3ebeb
Benjamin Herb <pingu@benjamin-herb.de>
2022-03-24 19:55:34 +0100

    Added argparser to input data directory

--- spotify_data_to_postgresql_db.py
+++ spotify_data_to_postgresql_db.py
@@ -10,77 +10,93 @@
 import json
 import psycopg2
 from datetime import datetime
+import argparse
 
-conn = psycopg2.connect(
-    database="DATABASE",
-    user='USER',
-    password='PASSWORD',
-    # host='localhost',
-    # port='5432'
-)
+def main(): 
+    data_dir = parse_arguments()
 
-print("IMPORTING EXTENDED DATA")
+    conn = psycopg2.connect(
+        database="DATABASE",
+        user='USER',
+        password='PASSWORD',
+        # host='localhost',
+        # port='5432'
+    )
 
-data = []
-for i in 0,1,2,3,4:
-    with open('data/endsong_' + str(i) + '.json') as f:
-        data += json.load(f)
+    print("IMPORTING EXTENDED DATA")
 
-cursor = conn.cursor()
+    data = []
+    for i in 0,1,2,3,4:
+        with open('data/endsong_' + str(i) + '.json') as f:
+            data += json.load(f)
 
-for i,d in enumerate(data):
-    endTime = d.get('ts')
-    msPlayed = d.get('ms_played')
-    trackName = d.get('master_metadata_track_name')
-    artistName = d.get('master_metadata_album_artist_name')
-    print(i, end='\r')
+    cursor = conn.cursor()
 
-    try:
-        cursor.execute("INSERT INTO streams VALUES( %s, %s, %s, %s)", (
-        endTime, msPlayed, trackName, artistName))
-        conn.commit()
-    except Exception as e:
-        print(e)
-        print(d)
-        exit()
+    for i,d in enumerate(data):
+        endTime = d.get('ts')
+        msPlayed = d.get('ms_played')
+        trackName = d.get('master_metadata_track_name')
+        artistName = d.get('master_metadata_album_artist_name')
+        print(i, end='\r')
 
-print("IMPORTING DATA")
-
-for i in 0,1:
-    with open('data/StreamingHistory' + str(i) + '.json') as f:
-        data += json.load(f)
-
-cursor.execute("""SELECT endTime FROM streams ORDER BY endTime DESC LIMIT 1;""")
-newest_entry = cursor.fetchall()[0][0]
-print(newest_entry)
-
-print("IMPORTING DATA")
-for i,d in enumerate(data):
-    endTime = d.get('endTime') + ':00'
-    msPlayed = d.get('msPlayed')
-    trackName = d.get('trackName')
-    artistName = d.get('artistName')
-    print(i, end='\r')
-    
-    endTime_comp = datetime.strptime(endTime, '%Y-%m-%d %H:%M:%S')
-
-    if (endTime_comp > newest_entry):
-        print(str(endTime_comp) + " is newer compared to " + str(newest_entry))
         try:
             cursor.execute("INSERT INTO streams VALUES( %s, %s, %s, %s)", (
             endTime, msPlayed, trackName, artistName))
-           
             conn.commit()
         except Exception as e:
             print(e)
             print(d)
             exit()
 
-print()
+    print("IMPORTING DATA")
 
-conn.close()
+    for i in 0,1:
+        with open('data/StreamingHistory' + str(i) + '.json') as f:
+            data += json.load(f)
+
+    cursor.execute("""SELECT endTime FROM streams ORDER BY endTime DESC LIMIT 1;""")
+    newest_entry = cursor.fetchall()[0][0]
+    print(newest_entry)
+
+    print("IMPORTING DATA")
+    for i,d in enumerate(data):
+        endTime = d.get('endTime') + ':00'
+        msPlayed = d.get('msPlayed')
+        trackName = d.get('trackName')
+        artistName = d.get('artistName')
+        print(i, end='\r')
+        
+        endTime_comp = datetime.strptime(endTime, '%Y-%m-%d %H:%M:%S')
+
+        if (endTime_comp > newest_entry):
+            print(str(endTime_comp) + " is newer compared to " + str(newest_entry))
+            try:
+                cursor.execute("INSERT INTO streams VALUES( %s, %s, %s, %s)", (
+                endTime, msPlayed, trackName, artistName))
+               
+                conn.commit()
+            except Exception as e:
+                print(e)
+                print(d)
+                exit()
+
+    print()
+
+    conn.close()
 
 
 
 
+def parse_arguments():
+    
+    parser = argparse.ArgumentParser(description='Imports Spotify data into a postgres database')
+    parser.add_argument("-d", "--data", required=True,
+       help="Data Directory with Spotify .json Files")
+    
+    args = vars(parser.parse_args())
+    data_dir = args['data']
 
+    return data_dir 
+
+if __name__=="__main__":
+    main()
