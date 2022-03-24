de15eacfcd8111139cb35f3ad6dc1bbb7ccbac34
Benjamin Herb <pingu@benjamin-herb.de>
2022-03-24 19:47:27 +0100

    Combined spotify extended + normal data import

--- spotify_data_to_postgresql_db.py
+++ spotify_data_to_postgresql_db.py
@@ -1,4 +1,4 @@
-# Setup Table in postgresdb
+# Setup Table in postgres
 
 # CREATE TABLE streams(
 #   endTime date,
@@ -9,13 +9,7 @@
 
 import json
 import psycopg2
-
-data = []
-
-for i in 0,1,2,3,4:
-    with open('data/endsong_' + str(i) + '.json') as f:
-        data += json.load(f)
-
+from datetime import datetime
 
 conn = psycopg2.connect(
     database="DATABASE",
@@ -25,8 +19,15 @@ conn = psycopg2.connect(
     # port='5432'
 )
 
+print("IMPORTING EXTENDED DATA")
+
+data = []
+for i in 0,1,2,3,4:
+    with open('data/endsong_' + str(i) + '.json') as f:
+        data += json.load(f)
+
 cursor = conn.cursor()
-print("IMPORTING DATA")
+
 for i,d in enumerate(data):
     endTime = d.get('ts')
     msPlayed = d.get('ms_played')
@@ -43,7 +44,43 @@ for i,d in enumerate(data):
         print(d)
         exit()
 
+print("IMPORTING DATA")
+
+for i in 0,1:
+    with open('data/StreamingHistory' + str(i) + '.json') as f:
+        data += json.load(f)
+
+cursor.execute("""SELECT endTime FROM streams ORDER BY endTime DESC LIMIT 1;""")
+newest_entry = cursor.fetchall()[0][0]
+print(newest_entry)
+
+print("IMPORTING DATA")
+for i,d in enumerate(data):
+    endTime = d.get('endTime') + ':00'
+    msPlayed = d.get('msPlayed')
+    trackName = d.get('trackName')
+    artistName = d.get('artistName')
+    print(i, end='\r')
+    
+    endTime_comp = datetime.strptime(endTime, '%Y-%m-%d %H:%M:%S')
+
+    if (endTime_comp > newest_entry):
+        print(str(endTime_comp) + " is newer compared to " + str(newest_entry))
+        try:
+            cursor.execute("INSERT INTO streams VALUES( %s, %s, %s, %s)", (
+            endTime, msPlayed, trackName, artistName))
+           
+            conn.commit()
+        except Exception as e:
+            print(e)
+            print(d)
+            exit()
+
 print()
+
 conn.close()
+
+
+
 
 
