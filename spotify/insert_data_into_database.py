044e863cccb3a0817bdb1eeaf5ea6b3a6833f77c
Benjamin Herb <pingu@benjamin-herb.de>
2022-03-24 19:31:15 +0100

    Updated insert scripts

--- spotify/insert_data_into_database.py
+++ spotify/insert_data_into_database.py
@@ -17,48 +17,17 @@ conn = psycopg2.connect(
 )
 
 cursor = conn.cursor()
-
+print("IMPORTING DATA")
 for i,d in enumerate(data):
-    ts = d.get('ts')
-    username = d.get('username')
-    platform =  d.get('platform')
-    ms_played = d.get('ms_played')
-    conn_country = d.get('conn_country')
-    ip_addr_decrypted = d.get('ip_addr_decrypted')
-    user_agent_decrypted = d.get('user_agent_decrypted')
-    master_metadata_track_name = d.get('master_metadata_track_name')
-    master_metadata_album_artist_name = d.get('master_metadata_album_artist_name')
-    master_metadata_album_album_name = d.get('master_metadata_album_album_name')
-    spotify_track_uri = d.get('spotify_track_uri')
-    episode_name = d.get('episode_name')
-    episode_show_name = d.get('episode_show_name')
-    spotify_episode_uri = d.get('spotify_episode_uri')
-    reason_start = d.get('reason_start')
-    reason_end = d.get('reason_end')
-    shuffle = d.get('shuffle')
-    skipped = d.get('skipped')
-    offline = d.get('offline')
-    offline_timestamp = d.get('offline_timestamp')
-    incognito_mode = d.get('incognito_mode')
-    print(i)
+    endTime = d.get('ts')
+    msPlayed = d.get('ms_played')
+    trackName = d.get('master_metadata_track_name')
+    artistName = d.get('master_metadata_album_artist_name')
+    print(i, end='\r')
 
     try:
-        cursor.execute("INSERT INTO streams VALUES( \
-            %s, %s, %s, \
-            %s, %s, %s, \
-            %s, %s, %s, \
-            %s, %s, %s, \
-            %s, %s, %s, \
-            %s, %s, %s, \
-            %s, %s, %s)", (
-            ts, username, platform, 
-            ms_played, conn_country, ip_addr_decrypted, 
-            user_agent_decrypted, master_metadata_track_name, master_metadata_album_artist_name, 
-            master_metadata_album_album_name, spotify_track_uri, episode_name, 
-            episode_show_name, spotify_episode_uri, reason_start, 
-            reason_end, shuffle, skipped, 
-            offline, offline_timestamp, incognito_mode
-            ))
+        cursor.execute("INSERT INTO streams VALUES( %s, %s, %s, %s)", (
+        endTime, msPlayed, trackName, artistName))
         
         conn.commit()
     except Exception as e:
@@ -66,6 +35,7 @@ for i,d in enumerate(data):
         print(d)
         exit()
 
+print()
 conn.close()
 
 
